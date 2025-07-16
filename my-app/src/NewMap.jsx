51b87ab12630c83f0325c4fd51d59948b57287b9
yiga nicholas <yiganicholas1@gmail.com>
2025-07-16 08:17:47 +0300

    map icon

--- my-app/src/NewMap.jsx
+++ my-app/src/NewMap.jsx
@@ -19,7 +19,16 @@ import Battery20Icon from '@mui/icons-material/Battery20';
 import BatteryAlertIcon from '@mui/icons-material/BatteryAlert';
 import MasksIcon from '@mui/icons-material/Masks';
 
-// Fix for default marker icons in Leaflet with Webpack
+// Create a custom icon
+const customIcon = new L.Icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 // Component to handle map view changes
 function MapView({ center }) {
@@ -182,7 +191,7 @@ export default function NewMap() {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position}>
+            <Marker position={position} icon={customIcon}>
               <Popup>
                 Asthma Monitor Location<br/>{locationName}
               </Popup>
